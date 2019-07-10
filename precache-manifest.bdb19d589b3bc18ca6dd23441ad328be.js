6d4b16c0fadabe681c78914ed26fa841bbb2e606
Adrian Velazquez <avelazquez585@gmail.com>
2019-07-10 14:09:01 -0400

    Updates

---
+++ precache-manifest.bdb19d589b3bc18ca6dd23441ad328be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42d219633818bc78369c2b0213982b3e",
+    "url": "/React-app-contact/index.html"
+  },
+  {
+    "revision": "175a54a9b0455cd53587",
+    "url": "/React-app-contact/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/React-app-contact/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "175a54a9b0455cd53587",
+    "url": "/React-app-contact/static/js/main.d4ed734f.chunk.js"
+  },
+  {
+    "revision": "acd5f76521ee173b95ad",
+    "url": "/React-app-contact/static/js/runtime~main.0bfa8b63.js"
+  }
+]);
